2ff9ca6d7edf02e559485070d53ac9aa7485a9cc
Etienne Klepal <etienne.klepal@gmail.com>
2023-12-25 23:59:37 +0100

    part 2 done

--- day_01/index.js
+++ day_01/index.js
@@ -35,4 +35,34 @@ async function partOne() {
   }
 }
 
-partOne();
+async function partTwo() {
+  try {
+    const rawData = await fs.readFile("input.txt", { encoding: "utf8" });
+    let parsedData = rawData;
+    lettersDigits.forEach((number, index) => {
+      parsedData = parsedData.replaceAll(
+        number,
+        `${number[0]}${index + 1}${number[number.length - 1]}`
+      );
+    });
+    const array = parsedData.split("\n");
+    array.pop();
+
+    const regex = /[0-9]/g;
+    const numbersArray = array.map((el) => el.match(regex));
+    const resultArray = numbersArray.map((el) =>
+      parseInt(el[0] + el[el.length - 1])
+    );
+
+    const sum = resultArray.reduce(function (a, b) {
+      return a + b;
+    });
+
+    console.log(sum);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// partOne();
+partTwo();
